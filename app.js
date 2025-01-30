6b752f4aba9bfd0a99650b9a9133510746b4a1e5
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2025-01-30 07:27:44 +0100

    created a get request for a public API

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {StatusCodes} = require('http-status-codes');
+const cors =  require('cors');
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.get('/',  (req, res) => {
+  const response = {
+    email: 'adebayoabbdqohar001@gmail.com',
+    current_datetime: new Date().toISOString(),
+    github_url: 'https://github.com/AbdulQohar1/HNG12-Zero.git'
+  };
+
+  res.status(StatusCodes.OK).json(response);
+})
+
+app.listen(port , () => {
+  console.log(`Listening on port: ${port}`);
+});
